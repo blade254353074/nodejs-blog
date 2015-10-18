3c9860343e6eb06741ba95dad9a4390ce502dc3a
SebastianBlade <blade254353074@hotmail.com>
2015-10-19 01:53:30 +0800

    add ccap captcha creater add captcha function to /admin/login

--- routes/admin.js
+++ routes/admin.js
@@ -1,8 +1,32 @@
 var express = require('express'),
     router = express.Router(),
-    crypto = require('crypto');
+    crypto = require('crypto'),
+    ccap = require('ccap'),
+    randomString = require('random-string');
 
 var Admin = require('../models/admin/user').Admin;
+
+// 获取验证码
+router.get('/captcha', function(req, res, next) {
+    var captcha = ccap({
+        width: 130,
+        height: 50,
+        offset: 30,
+        quality: 100,
+        fontsize: 40,
+        generate: function() {
+            var str = randomString({
+                length: 4
+            });
+            return str;
+        }
+    });
+    var cptArr = captcha.get();
+    // 将生成的验证码存到session中
+    req.session.captcha = cptArr[0].toLocaleLowerCase();
+    res.write(cptArr[1]);
+    res.end();
+});
 
 // 登录界面
 router.get('/login', function(req, res, next) {
@@ -10,27 +34,42 @@ router.get('/login', function(req, res, next) {
 });
 
 router.post('/login', function(req, res, next) {
+    var captcha = req.body.captcha,
+        username = req.headers.username,
+        password = req.headers.password;
+
+    if (!captcha || captcha.toLocaleLowerCase() !== req.session.captcha) {
+        return res.json({
+            state: false,
+            captcha: false
+        });
+    }
+    if (!username || !password) {
+        return res.json({
+            state: false
+        });
+    }
     Admin.findOne({
-        username: req.headers.username
+        username: username
     }, function(err, user) {
         var shasum;
         // 有此用户
         if (user) {
             // 用crypto sha1加密比较密码
             shasum = crypto.createHash('sha1');
-            shasum.update(req.headers.password);
+            shasum.update(password);
             if (shasum.digest('hex') === user.password) {
                 // 验证通过，添加_id到session
                 req.session.user_id = user._id
-                res.json({
+                return res.json({
                     state: true
                 });
             }
-        } else {
-            res.json({
-                state: false
-            });
         }
+        // 用户名或密码错误
+        res.json({
+            state: false
+        });
     });
 });
 
